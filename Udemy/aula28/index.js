4f4103d4ccb33ba1c29598963cabc6a7d42511a6
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2022-09-23 13:46:45 -0300

    aula 27

---
+++ Udemy/aula28/index.js
@@ -0,0 +1,8 @@
+/*
+const tresHoras = 60 * 60 * 3 * 1000;
+const umDia = 60 * 60 * 24 * 1000;
+const data = new Date(0 + tresHoras - umDia); // 01/01/1970 timestamp unix ou época unix
+*/
+const data = new Date(2022, 3, 20, 12, 30 ); // a, m, d, h, M, s, ms
+const data 
+console.log(data.toString());

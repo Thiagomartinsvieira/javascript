4ce822551c5ebb9dfa2859b6f7f94557e10bab2d
Thiagomartinsvieira <thiagoalagadico@gmail.com>
2023-01-07 11:53:15 -0300

    aula 39 em andamento

---
+++ Udemy/aula39/assets/js/main.js
@@ -0,0 +1,23 @@
+function random(min, max){
+  const r = Math.random() * (max - min) + min;
+  return r;
+}
+
+
+
+
+
+
+
+
+/*
+const nome = 'Thiago';
+let i = 0;
+
+while (i < nome.length) {
+    console.log(nome[i]);
+    i++;
+}
+
+console.log('Segue a vida...');
+*/
